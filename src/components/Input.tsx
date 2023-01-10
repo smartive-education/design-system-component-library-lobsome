9967d09b3a63c4717b414be477eaab40a49f68c6
Alexander Widmer <alexander.widmer@me.com>
2023-01-10 18:15:33 +0100

    refactor(input): use react "useId" instead of name property

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,24 +1,24 @@
-import React, { HTMLAttributes, ReactNode } from 'react';
+import React, {HTMLAttributes, ReactNode, useId} from 'react';
 
 export interface InputProps extends HTMLAttributes<HTMLInputElement> {
     events?: { [key: string]: (e: Event) => void };
     label?: string;
-    name?: string;
     children?: ReactNode;
 }
 
-export const Input = ({ label = '', events, name, children, ...rest }: InputProps) => {
+export const Input = ({ label = '', events, children, ...rest }: InputProps) => {
+    const id = useId();
     return (
         <div className="font-semibold">
             {label && (
-                <label htmlFor={name} className="block mb-0 text-slate-700 text-base">
+                <label htmlFor={id} className="block mb-0 text-slate-700 text-base">
                     {label}
                 </label>
             )}
             <div className="relative flex items-center">
                 <input
                     type="text"
-                    id={name}
+                    id={id}
                     className="py-4 pl-4 pr-10 border border-slate-300 rounded-lg leading-4 hover:border-violet-600 h-12 font-medium outline-transparent focus:outline-violet-600"
                     {...rest}
                     {...events}
